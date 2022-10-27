d9b5a3d9717dcf5f24af438319b5e836486dd0be
brandon-w1205 <bwu1205@gmail.com>
2022-10-26 22:03:04 -0700

    adds hook return and movement functions

--- components/tetris/App.tsx
+++ components/tetris/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { createStage } from './gameHelpers'
 
+// Custom hooks
+import { useInterval } from './hooks/useInterval'
+import { usePlayer } from './hooks/usePlayer'
+import { useStage } from './hooks/useStage'
+
 
 import Stage from './stage/Stage'
 import Display from './display/Display'
@@ -11,10 +16,28 @@ import StartButton from './startbutton/StartButton'
 import { StyledTetrisWrapper, StyledTetris } from './App.styles';
 
 const App: React.FC = () => {
-    const [dropTime, setDropTime] = React.useState<null | number>(null);
-    const [gameOver, setGameOver] = React.useState(true)
+  const [dropTime, setDropTime] = React.useState<null | number>(null);
+  const [gameOver, setGameOver] = React.useState(true)
 
+  const { player, updatePlayerPos, resetPlayer } = usePlayer()
+  const { stage, setStage } = useStage(player, resetPlayer)
 
+  const movePlayer = (dir: number) => {
+    updatePlayerPos({ x: dir, y: 0, collided: false})
+  }
+
+  const move = ({ keyCode, repeat }: { keyCode: number, repeat: boolean}): void => {
+    if (keyCode === 37) {
+      movePlayer(-1);
+    } else if (keyCode === 39) {
+      movePlayer(1);
+    } else if (keyCode === 40) {
+      if(repeat) {
+        return
+      }
+      setDropTime(30)
+    }
+  }
 
   return (
     <StyledTetrisWrapper role='button' tabIndex={0}>
