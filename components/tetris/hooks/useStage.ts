b1d7e9eaa482e8b3cbfd767dd96b339e77dfefa2
brandon-w1205 <bwu1205@gmail.com>
2022-10-27 16:10:00 -0700

    adds clearing functionality

--- components/tetris/hooks/useStage.ts
+++ components/tetris/hooks/useStage.ts
@@ -1,3 +1,4 @@
+import AnchorLink from 'antd/lib/anchor/AnchorLink';
 import React from 'react'
 import { createStage } from '../gameHelpers';
 
@@ -12,9 +13,27 @@ export type STAGE = STAGECELL[][]
 
 export const useStage = (player: player, resetPlayer: () => void) => {
     const [stage, setStage] = React.useState(createStage());
+    const [rowsCleared, setRowsCleared] = React.useState(0)
 
     React.useEffect(() => {
         if (!player.pos) return;
+
+        setRowsCleared(0)
+
+        const sweepRows = (newStage: STAGE): STAGE => {
+            return newStage.reduce((stack, row) => {
+                // if no 0, row is full and needs to be cleared
+                if (row.findIndex(cell => cell[0] === 0) === -1) {
+                    setRowsCleared(prev => prev + 1)
+                    // create empty row at beginning of array to push tetris piece down instead of clearing row
+                    stack.unshift(new Array(newStage[0].length).fill([0, 'clear']) as STAGECELL[])
+                    return stack
+                }
+
+                stack.push(row)
+                return stack
+            }, [] as STAGE)
+        }
 
         const updateStage = (prevStage: STAGE): STAGE => {
             // Flushing the Stage
@@ -34,6 +53,7 @@ export const useStage = (player: player, resetPlayer: () => void) => {
             
             if (player.collided) {
                 resetPlayer()
+                return sweepRows(newStage)
             }
 
             return newStage
