f57d62e8e00add9924550e93b01e933b18b4c1a3
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-29 12:08:32 -0700

    working on pause functionality

--- components/sudoku/startSection.tsx
+++ components/sudoku/startSection.tsx
@@ -5,14 +5,20 @@ import styles from '../../styles/sudoku/Board.module.css'
 type Props = {
     handleStartGame: any
     gameStarted : string
+    newGame: any
+    hasGame: boolean
 }
 
-const StartSection = ({handleStartGame, gameStarted}: Props) => {
+const StartSection = ({handleStartGame, gameStarted, newGame, hasGame}: Props) => {
 
+  const start = <Button type='primary' onClick={handleStartGame}>{gameStarted}</Button>
+  const cont = <Button type='primary' onClick={newGame}>{gameStarted}</Button>
+ 
   return (
     
     <div className={styles.controlButtonConstraints} >
-        <Button type='primary' onClick={handleStartGame}>{gameStarted}</Button>
+      {hasGame? cont: start}
+        
         <Button type='primary' >Check Game</Button>
     </div>
   )
