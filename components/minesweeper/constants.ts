679c1173181522716fd6e3aad960dbbe743185ed
iandab47 <dabinett.ian@gmail.com>
2022-10-29 14:03:49 -0500

    adds more styling to MS gameboard

--- components/minesweeper/constants.ts
+++ components/minesweeper/constants.ts
@@ -1,3 +1,3 @@
-export const BOARD_WIDTH = 16
-export const BOARD_HEIGHT = 30
-export const BOARD_MINES = 99
+export const BOARD_WIDTH = 9
+export const BOARD_HEIGHT = 9
+export const BOARD_MINES = 10
