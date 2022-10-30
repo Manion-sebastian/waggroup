ab51f0ab456bd852e0a9cd95bce2ad7a284fdd96
brandon-w1205 <bwu1205@gmail.com>
2022-10-29 22:01:04 -0700

    adds bit of styling

--- components/tetris/display/Display.styles.ts
+++ components/tetris/display/Display.styles.ts
@@ -7,14 +7,15 @@ type Props = {
 export const StyledDisplay = styled.div<Props>`
   box-sizing: border-box;
   display: flex;
+  justify-content: center;
   align-items: space-between;
-  margin: 0 0 20px 0;
-  padding: 20px;
+  margin: 0 0 10px 0;
+  padding: 0px;
   border: 2px solid #777;
   min-height: 20px;
   width: 120px;
   border-radius: 10px;
-  color: ${props => (props.gameOver ? 'red' : '#999')};
+  color: ${props => (props.gameOver ? 'red' : 'white')};
   background: #000;
   font-family: Arial, Helvetica, sans-serif;
   font-size: 0.8rem;
