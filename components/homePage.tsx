f60011c3fb27ac77f16e076787a87d6728337846
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-30 16:15:23 -0700

    cont

--- components/homePage.tsx
+++ components/homePage.tsx
@@ -1,24 +1,46 @@
 import React from 'react'
-import  Link  from 'next/link'
+import { Divider, Collapse } from 'antd'
+import styles from '../styles/baseSite/About.module.css'
 
 type Props = {}
 
+const { Panel } = Collapse
+
 const HomePage = (props: Props) => {
   return (
-    <div>
-      <h1>About WAG</h1>
-      <div>
-        <h2>Welcome to WAG, a Capstone Project from the developers that brought you... stuff.</h2>
-        <p>We all really like these games and so we wanted to make a place where we could easily play them.</p>
-      </div>
-      <div>
-        <h3>Do I need an account?</h3>
-        <p>No!, we made this site so you can play all these games freely, however if you want to track your scores feel free to make an account. scores are easily viewed on your profile.</p>
+    <div className={styles.container}>
+
+      <div className={styles.aboutWhole}>
+        <div>
+          <h1 className={styles.headers}>Welcome to WAG!</h1>
+          <p className={styles.content}>a Capstone Project from the developers that brought you Global Keebs, CheapEGames, and d'ora</p>
+          <h2 className={styles.headers}>Why did we make WAG?</h2>
+          <p className={styles.content}>We all enjoy these types of games so we made WAG to see if we could implement a site that we've all visited before from scratch.</p>
+          <h3 className={styles.headers}>Do I need an account?</h3>
+          <p className={styles.content}>No!, we made this site so you can play all these games freely, however if you want to track your scores feel free to make an account. scores are easily viewed on your profile.</p>
+        </div>
+        <h4 className={styles.headers}>Instructions</h4>
+        <Collapse expandIconPosition='end' bordered={false} accordion={true}>
+          <Panel className={styles.headers} header={"Minesweeper"} key='1'>
+            <p className={styles.content}>
+              Click on the squares to ...
+            </p>
+          </Panel>
+          <Panel className={styles.headers} header={"Sudoku"} key='2'>
+          <p className={styles.content}>
+            A standard game of Sudoku, Click on cells to select it, and then click a button to insert the number into the cell.
+          </p>
+          </Panel>
+          <Panel className={styles.headers} header={"Tetris"} key='3'>
+          <p className={styles.content}>
+            A standard game of Tetris
+          </p>
+          </Panel>
+        </Collapse>
+
+        <Divider />
 
       </div>
-
-
-
     </div>
   )
 }
