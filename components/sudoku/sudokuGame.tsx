caee17b3ca4447b83e45bdbfdb8955d5bc61e093
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-30 12:48:54 -0700

    puzzles are fixed, validate works

--- components/sudoku/sudokuGame.tsx
+++ components/sudoku/sudokuGame.tsx
@@ -5,20 +5,19 @@ import StatusBar from './statusBar'
 import styles from '../../styles/sudoku/Board.module.css'
 import { makepuzzle, solvepuzzle, ratepuzzle } from 'sudoku'
 import StartSection from './startSection'
+import sudoku from 'sudoku'
 
 
 
 type Props = {}
 
 const SudokuGame = (props: Props) => {
-  // const tempBoard = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null]
-
   const tempBoardTwo = ['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',]
   
   const [currentPuzzle, setCurrentPuzzle] = useState<number[] | string[]>(tempBoardTwo)
   const [hasGame, setHasGame] = useState<boolean>(false)
   const [currentCell, setCurrentCell] = useState()
-  const [solvedPuzzle, setSolvedPuzzle] = useState<number[]>([])
+  const [solvedPuzzle, setSolvedPuzzle] = useState<number[] | string[]>([])
   const [currentTimer, setCurrentTimer] = useState<number>(0)
   const [paused, setPaused] = useState<boolean>(false)
   let [tranferPuzzle, setTransferPuzzle] = useState<number[] | string[]>([])
@@ -38,16 +37,35 @@ const SudokuGame = (props: Props) => {
 
  // when a new input is pushed in -- run a check on the string. if the new check is not the correct tally the error in secret. when the board is completed updated tally to show errors. 
 
+ function changePuzzle(nput : string[] | number[]) {
+  let fixedPuzzle = nput
+  for (let i = 0; i < 81; i++) {
+    if (fixedPuzzle[i] === 0) {
+      fixedPuzzle[i] = 9 
+    }
+  }
+  return fixedPuzzle
+ }
+
+
 const handleStartGame = () => {
-  setCurrentPuzzle( makepuzzle() )
-  setSolvedPuzzle(solvepuzzle(currentPuzzle))
+  const newPuzz = sudoku.makepuzzle()
+  const solvedPuzz = sudoku.solvepuzzle(newPuzz)
+  const fixedPuzz = changePuzzle(newPuzz)
+  const fixedSolve = changePuzzle(solvedPuzz)
+  setCurrentPuzzle(fixedPuzz)
+  setSolvedPuzzle(fixedSolve)
   setHasGame(true)
   
 }
 
 const handleNewGame = () => {
-  setCurrentPuzzle( makepuzzle() )
-  setSolvedPuzzle(solvepuzzle(currentPuzzle))
+  const newPuzz = sudoku.makepuzzle()
+  const solvedPuzz = sudoku.solvepuzzle(newPuzz)
+  const fixedPuzz = changePuzzle(newPuzz)
+  const fixedSolve = changePuzzle(solvedPuzz)
+  setCurrentPuzzle(fixedPuzz)
+  setSolvedPuzzle(fixedSolve)
   setCurrentTimer(0)
 
 }
@@ -68,11 +86,13 @@ const handlePauseGame = () => {
 }
 
 const handleValidate = () => {
-  for(let i = 0; i < 81; i++) {
-    if (currentPuzzle[i] === solvedPuzzle[i]) {
-      console.log(true)
+  for ( let i = 0; i < 81; i++) {
+    if (currentPuzzle[i] !== '') {
+      if (currentPuzzle[i] == solvedPuzzle![i]) {
+        console.log(true)
+      }
     } else {
-      console.log(false)
+      continue
     }
   }
 }
@@ -121,7 +141,7 @@ const handleStartButton = () => {
       <StatusBar time={currentTimer} handlePause={handlePauseGame} paused={paused} />
       <Board game={currentPuzzle}  grabInfo={handleGrabObj} />
       <div className={styles.boardConstUse}>
-        <StartSection gameStarted={handleStartButton()} newGame={handleNewGame} hasGame={hasGame} handleStartGame={handleStartGame} /> 
+        <StartSection gameStarted={handleStartButton()} newGame={handleNewGame} handleVal={handleValidate} hasGame={hasGame} handleStartGame={handleStartGame} /> 
         <ControlBar handleInput={handleInput}  />
       </div>
     </div>
