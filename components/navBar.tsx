88913c0fc68e260de5aefc6fed4c53d0d9423011
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-29 21:19:38 -0700

    fixed nav

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -8,13 +8,13 @@ const NavBar = () => {
 
   const items = [
     {label:<Link href={'/'}>Home</Link>, key:'home'},
-    {label:<Link href={'/about'}>About</Link>, key:'about'},
-    {label:<Link href={'/auth'}>Login or Register</Link>, key:'authentication'},
     {label:'Games', key:'games',children:[
       {label:<Link href={'/minesweeper'}>Minesweeper</Link>,key:'Minesweeper'},
       {label:<Link href={'/sudoku'}>Sudoku</Link>,key:'Sudoku'},
       {label:<Link href={'/tetris'}>Tetris</Link>,key:'Tetris'},
-    ]}
+    ]},
+    {label:<Link href={'/auth'}>Login or Register</Link>, key:'authentication'},
+    {label:<Link href={'/about'}>About</Link>, key:'about'},
   ]
     // <div>
     //     <Link href={'/'}>Home</Link>{" "} |
