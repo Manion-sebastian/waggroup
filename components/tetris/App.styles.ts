cdda52433be37971f13d24bb4d208b2a615e51b7
iandab47 <dabinett.ian@gmail.com>
2022-10-30 18:16:19 -0500

    upscales tetris to be max size of screen

--- components/tetris/App.styles.ts
+++ components/tetris/App.styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const StyledTetrisWrapper = styled.div`
   width: 100%;
   height: calc(100vh - 46px);
+  max-height
   overflow: hidden;
   outline: none;
 `;
@@ -11,13 +12,13 @@ export const StyledTetris = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 10px;
+  width: 100%;
+  max-width: 30rem;
   margin: 0 auto;
   
   .display {
     display: flex;
     justify-content: space-between;
-    width: 380px;
-    padding: 0 0 -10px 0;
+    width: max-content;
   }
 `;
