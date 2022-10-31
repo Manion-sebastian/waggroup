a1862b3008818cd3a6989fb5d71f41df48f959a9
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-30 19:30:22 -0700

    split pages

---
+++ pages/login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginPage from '../components/authentication/loginPage'
+
+type Props = {}
+
+const Login = (props: Props) => {
+  return (
+    <div>
+        <LoginPage />
+    </div>
+  )
+}
+
+export default Login
