4e5c9868d3775f6ac06cce495d482322a09c2a1f
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-31 08:09:08 -0700

    testing navbar

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -33,21 +33,21 @@ const NavBar = () => {
 
 
   // w nestled elements
-  const items = [
-    {label:<Link href={'/'}>Home</Link>, key:'home', icon: <HomeOutlined />},
-    {label:'Games', key:'games', icon: <MenuOutlined />, children:[
-      {label:<Link href={'/minesweeper'}>Minesweeper</Link>,key:'Minesweeper', icon: <FlagOutlined />  },
-      {label:<Link href={'/sudoku'}>Sudoku</Link>,key:'Sudoku', icon:<NumberOutlined />},
-      {label:<Link href={'/tetris'}>Tetris</Link>,key:'Tetris', icon:<BuildOutlined />},
-    ]},
-    {label:<Link href={'/profile'}>Profile</Link>, key:'profile', icon:<UserOutlined />},
-    {label:'Auth', key:'auth', icon:<UserOutlined />, children: [
-      {label:<Link href={'/login'}>Login</Link>, key:'login', icon:<UserOutlined />},
-      {label:<Link href={'/register'}>Register</Link>, key:'register', icon:<UserOutlined />},
-      {label:<button style={{border: 'none'}} onClick={handleLogout}>Log Out</button>, key:'logout', icon:<UserOutlined />},
-    ]},
-    {label:<Link href={'/about'}>About</Link>, key:'about', icon: <QuestionCircleOutlined />},
-  ]
+  // const items = [
+  //   {label:<Link href={'/'}>Home</Link>, key:'home', icon: <HomeOutlined />},
+  //   {label:'Games', key:'games', icon: <MenuOutlined />, children:[
+  //     {label:<Link href={'/minesweeper'}>Minesweeper</Link>,key:'Minesweeper', icon: <FlagOutlined />  },
+  //     {label:<Link href={'/sudoku'}>Sudoku</Link>,key:'Sudoku', icon:<NumberOutlined />},
+  //     {label:<Link href={'/tetris'}>Tetris</Link>,key:'Tetris', icon:<BuildOutlined />},
+  //   ]},
+  //   {label:<Link href={'/profile'}>Profile</Link>, key:'profile', icon:<UserOutlined />},
+  //   {label:'Auth', key:'auth', icon:<UserOutlined />, children: [
+  //     {label:<Link href={'/login'}>Login</Link>, key:'login', icon:<UserOutlined />},
+  //     {label:<Link href={'/register'}>Register</Link>, key:'register', icon:<UserOutlined />},
+  //     {label:<button style={{border: 'none'}} onClick={handleLogout}>Log Out</button>, key:'logout', icon:<UserOutlined />},
+  //   ]},
+  //   {label:<Link href={'/about'}>About</Link>, key:'about', icon: <QuestionCircleOutlined />},
+  // ]
 
   const loggedOutItems = [
     {label:<Link href={'/'}>Home</Link>, key:'home', icon: <HomeOutlined />},
@@ -64,6 +64,17 @@ const NavBar = () => {
     ]},
     {label:<Link href={'/about'}>About</Link>, key:'about', icon: <QuestionCircleOutlined />},
   ]
+
+  // test
+  const items = [
+    { label: 'item 1', key: 'item-1' }, // remember to pass the key prop
+    { label: 'item 2', key: 'item-2' }, // which is required
+    {
+      label: 'sub menu',
+      key: 'submenu',
+      children: [{ label: 'item 3', key: 'submenu-item-1' }],
+    },
+  ];
 
   // const items = [
   //   {label:<Link href={'/'}>Home</Link>, key:'home', icon: <HomeOutlined />},
