ad0531b4214507f768ce0dcdd7f324284e2f0348
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-30 19:57:48 -0700

    registration form updated

--- components/authentication/registration.tsx
+++ components/authentication/registration.tsx
@@ -6,27 +6,67 @@ import jwt_decode from 'jwt-decode'
 
 
 
-type Props = {}
+type Props = {
+    currentUser: any
+    setCurrentUser: any
+}
 
-const Registration = (props: Props) => {
+const Registration = ({currentUser, setCurrentUser}: Props) => {
+
+    const [name, setName] = useState('')
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [msg, setMsg] = useState('')
+
+    const handleSubmit = async (e:any) => {
+        e.preventDefault()
+        try {
+            const reqBody = {
+                name,
+                username,
+				email, 
+				password
+			}
+            // console.log(reqBody)
+			// const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/register`, reqBody)
+			const { token } = response.data
+			localStorage.setItem('jwt', token)
+			const decoded = jwt_decode(token)
+			setCurrentUser(decoded)
+
+		} catch (err : any) {
+            console.warn(err)
+			if (err.response) {
+			    setMsg(err.response.data.msg)
+
+            }
+        }
+    }
+
+    // add navigate
+    // if (currentUser) {
+	// 	return <Navigate to="/profile" />
+	// }
+
   return (
     <div className={styles.authForms}>
-        <form action="/register" method='POST'>
+        <form onSubmit={handleSubmit}>
             <div className={styles.authLabelSection}>
                 <label className={styles.authLabel} htmlFor="name">Name:</label>
-                <input className={styles.authInput} name='name' id='name' type="text" required placeholder='Enter Name'/>
+                <input className={styles.authInput} name='name' id='name' type="text" required placeholder='Enter Name' value={name} onChange={e => setName(e.target.value)}/>
             </div>
             <div className={styles.authLabelSection}>
                 <label className={styles.authLabel} htmlFor='username'>Username:</label>
-                <input className={styles.authInput} name='username' id='username' type='text' required placeholder='Enter Username' />
+                <input className={styles.authInput} name='username' id='username' type='text' required placeholder='Enter Username' value={username} onChange={e => setUsername(e.target.value)} />
             </div>
             <div className={styles.authLabelSection}>
                 <label className={styles.authLabel} htmlFor="email">Email:</label>
-                <input className={styles.authInput} name='email' id='email' type="email" required placeholder='Enter Email' />
+                <input className={styles.authInput} name='email' id='email' type="email" required placeholder='Enter Email' value={email} onChange={e => setEmail(e.target.value)} />
             </div>
             <div className={styles.authLabelSection}>
                 <label className={styles.authLabel} htmlFor="password">Password:</label>
-                <input className={styles.authInput} name='password' id='password' type='password' required placeholder='Enter Password' />
+                <input className={styles.authInput} name='password' id='password' type='password' required placeholder='Enter Password' value={password} onChange={e => setPassword(e.target.value)} />
             </div>
             <button className={styles.authButton} type='submit'>Register</button>
         </form>
