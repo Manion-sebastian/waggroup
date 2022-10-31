c91013fdfac49976207485f80fc6cb58a154b628
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-31 08:36:57 -0700

    added ant design import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import NavBar from '../components/navBar'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import 'antd/dist/antd.css';
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
